4dd8dc73f0a9ab7e990bc104f4aa97776a3551e6
hasanraza38 <92hasanraza689@gmail.com>
2024-09-11 02:46:03 +0500

    updates

---
+++ src/layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Navbar from './components/header/navbar'
+import Footer from './components/footer/footer'
+
+const Layout = () => {
+  return (
+<>
+<Navbar/>
+<Outlet/>
+{/* <Footer/> */}
+</>
+
+)
+}
+
+export default Layout
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,56 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './layout.jsx'
+import Home from './screens/home.jsx'
+import About from './screens/about.jsx'
+import Services from './screens/services.jsx'
+import Contact from './screens/contact.jsx'
+
+const router= createBrowserRouter([
+  {
+    path:'/',
+    element:<Layout/>,
+    children:[
+      {
+        path:"home",
+        element:<Home/>
+      },
+      {
+        path:"about",
+        element:<About/>,
+        children:[
+          {
+            path:"",
+            element:<h1>person 1</h1>
+          },
+          {
+            path:"person2",
+            element:<h1>person 2</h1>
+          },
+          {
+            path:"person3",
+            element:<h1>person 3</h1>
+          },
+        ]
+      },
+      {
+        path:'services',
+        element:<Services/>
+      },
+      {
+        path:"contact",
+        element:<Contact/>
+      },
+      {
+        path:"*",
+        element:<h1>Noy Found!!!</h1>
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+  <RouterProvider router={router}></RouterProvider>
 )
